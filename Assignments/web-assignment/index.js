cd492f788ed030a8977d615284c0542c1f425990
Arshad-Saira <168642882+Arshad-Saira@users.noreply.github.com>
2024-07-18 06:35:56 -0400

    Web assignment part 1 code added

---
+++ Assignments/web-assignment/index.js
@@ -0,0 +1,114 @@
+[
+	{
+		book : "Reclaim Your Heart",
+		id: 1,
+		author: "Yasmin Mogahed",
+		price: 1499
+	},
+	{
+		book : "Rich Dad, Poor Dad",
+		id: 2,
+		author: "Robert Kiyosaki",
+		price: 999
+	},{
+		book : "The Psychology of Money",
+		id: 3,
+		author: "Morgan Housel",
+		price: 1199
+	},
+	{
+		book : "As You Like it",
+		id: 4,
+		author: "William Shakespeare",
+		price: 1599
+	},{
+		book : "It's Life",
+		id: 5,
+		author: "Robert Kiyosaki",
+		price: 999
+	},
+	{
+		book : "Five Truths",
+		id: 6,
+		author: "Morgan Housel",
+		price: 1599
+	},{
+		book : "Master your Mind",
+		id: 7,
+		author: "Anonymous",
+		price: 1499
+	},
+	{
+		book : "Fake People",
+		id: 8,
+		author: "William Shakespeare",
+		price: 1199
+	},{
+		book : "You may know",
+		id: 9,
+		author: "Jim Collins",
+		price: 999
+	},
+	{
+		book : "You are best",
+		id: 10,
+		author: "Anonymous",
+		price: 1999
+	},{
+		book : "Be You",
+		id: 11,
+		author: "Yasmin Mogahed",
+		price: 1670
+	},
+	{
+		book : "Treasure Island",
+		id: 12,
+		author: "Robert Louis",
+		price: 1449
+	},{
+		book : "Pride and Prejudice",
+		id: 13,
+		author: "Jane Austin",
+		price: 1149
+	},
+	{
+		book : "Adventures of Tom Sawyer",
+		id: 14,
+		author: "Mark Twain",
+		price: 1499
+	},{
+		book : "A Tale of Two Cities",
+		id: 15,
+		author: "Charles Dickens",
+		price: 1799
+	},
+	{
+		book : "Great Expections",
+		id: 16,
+		author: "Charles Dickens",
+		price: 1999
+	},{
+		book : "Oliver Twist",
+		id: 17,
+		author: "Charles Dickens",
+		price: 1499
+	},
+	{
+		book : "Return of King",
+		id: 18,
+		author: "Tolkien",
+		price: 1099
+	},{
+		book : "The Three Musketeers",
+		id: 19,
+		author: "Alexandre Dumas",
+		price: 1699
+	},
+	{
+		book : "War and Peace",
+		id: 20,
+		author: "Leo Tolstoy",
+		price: 1999
+	},
+	
+]
