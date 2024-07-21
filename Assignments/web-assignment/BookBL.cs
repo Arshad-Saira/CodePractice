20b2af7f81196b670d4016b005d2cd2907c577b3
Arshad-Saira <168642882+Arshad-Saira@users.noreply.github.com>
2024-07-21 15:49:39 +0500

    Web  API Assignment part03 code

---
+++ Assignments/web-assignment/BookBL.cs
@@ -0,0 +1,40 @@
+﻿using WebApplication1.DataAccessLayer;
+using WebApplication1.Models;
+
+namespace WebApplication1.BusinessLayer
+{
+    public class BookBL
+    {
+
+        private readonly BookDAL bookDAL = new BookDAL();
+        public List<Book> GetBooks()
+        {
+            return bookDAL.GetBooks();
+        }
+
+        public Book GetBook(int id)
+        {
+            return bookDAL.GetBook(id);
+        }
+
+        public void AddBook(Book book)
+        {
+            bookDAL.AddBook(book);
+        }
+
+        public void UpdateBook(int id, Book book)
+        {
+            var existingBook = bookDAL.GetBook(id);
+            if (existingBook != null)
+            {
+                book.Id = id; // Ensure the ID remains unchanged
+                bookDAL.UpdateBook(book);
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            bookDAL.DeleteBook(id);
+        }
+    }
+}
---
+++ Assignments/web-assignment/BookDAL.cs
@@ -0,0 +1,48 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.DataAccessLayer
+{
+    public class BookDAL
+    {
+        private static List<Book> books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Reclaim Your Heart", Author = "Yasmin Mogahed", Description = "Its Great Book" , ImageUrl = "/"}
+        };
+
+        public List<Book> GetBooks()
+        {
+            return books;
+        }
+
+        public Book GetBook(int id)
+        {
+            return books.FirstOrDefault(b => b.Id == id);
+        }
+        public void AddBook(Book book)
+        {
+            book.Id = books.Max(p => p.Id) + 1;
+            books.Add(book);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            var existingBook = books.FirstOrDefault(b => b.Id == book.Id);
+
+            if (existingBook != null)
+            {
+                existingBook.Title = book.Title;
+                existingBook.Author = book.Author;
+                existingBook.Description = book.Description;
+                existingBook.ImageUrl = book.ImageUrl;
+            }
+        }
+        public void DeleteBook(int id)
+        {
+            var book = books.First(b => b.Id == id);
+            if (book != null)
+            {
+                books.Remove(book);
+            }
+        }
+    }
+}
